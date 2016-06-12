ee7ecf394ff06d519b5f6fca4cc1710be6fe9bd1
tanyang <122074809@qq.com>
2016-06-12 16:12:03 +0800

    Initial Commit

---
+++ TYDownloadManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYDownloadManagerDemo
+//
+//  Created by tany on 16/6/12.
+//  Copyright © 2016年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYDownloadManagerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TYDownloadManagerDemo
+//
+//  Created by tany on 16/6/12.
+//  Copyright © 2016年 tany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
